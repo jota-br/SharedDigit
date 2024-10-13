f23c36acb8dbf9afbde8e0accc4b7b7db6cd60a0
João Carlos Roveda Ostrovski <ojcroveda@gmail.com>
2024-10-13 20:05:47 -0300

    ...

--- SharedDigit.java
+++ SharedDigit.java
@@ -1,22 +1,32 @@
 public class SharedDigit {
-    public static boolean hasSharedDigit(int x, int y) {
-        if((x < 10) || (y > 99) || (x > 99) || (y < 10)) {
+    public static void main(String[] args) {
+        System.out.println(hasSharedDigit(12,13));
+    }
+
+    public static boolean hasSharedDigit (int x, int y) {
+        if ((x > 99 || x < 10) || (y > 99 || y < 10)) {
             return false;
         }
 
-        int k = 0, j = 0;
-        while(x > 0) {
-            k = (x % 10);
-            x /= 10;
-
-            while(y > 0) {
-                j = (y % 10);
-                y /= 10;
-                if(k == j || x == j || x == y) {
+        int i = x;
+        int j = y;
+        while (true) {
+            while (true) { 
+                if (j == 0) {
+                    j = y;
+                    break;
+                }
+                if (i % 10 == j % 10) {
                     return true;
                 }
+                j /= 10;
             }
+            if (i == 0) {
+                break;
+            }
+            i /= 10;
         }
+
         return false;
     }
 }
